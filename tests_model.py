9ce0e03612dae140779f245ec96078f9c27c4f86
arnav-sys <programmeraggarwal@gmail.com>
2022-01-05 09:34:26 +0530

    changing test files

--- tests_model.py
+++ tests_model.py
@@ -11,14 +11,13 @@ import tensorflow
 import pytest
 from tensorflow import keras
 
-model = keras.models.load_model("../model.h5")
 
 #making predictions
 from PIL import Image
 from itertools import groupby
 
 'loading image'
-image = Image.open("../testing.png").convert("L")
+image = Image.open("./testing.png").convert("L")
 
 'resizing to 28 height pixels'
 w = image.size[0]
@@ -79,13 +78,15 @@ for x in range(0, num_of_elements):
 
 elements_array = np.array(elements_list)
 
-'reshaping to fit model input criteria'
 elements_array = elements_array.reshape(-1, 28, 28, 1)
 
-'predicting using the model'
-elements_pred =  model.predict(elements_array)
-predictions =  np.argmax(elements_pred, axis = 1)
-true_predictions = [ 9,  8, 10 , 7,  6, 13 , 5,  4, 11,  3,  2, 12,  1 , 0]
 
-from sklearn.metrics import accuracy_score
-assert accuracy_score(true_predictions, predictions) > 0.9
+def test_model():
+     model = keras.models.load_model("./model.h5")
+    
+     elements_pred =  model.predict(elements_array)
+     predictions =  np.argmax(elements_pred, axis = 1)
+     true_predictions = [ 9,  8, 10 , 7,  6, 13 , 5,  4, 11,  3,  2, 12,  1 , 0]
+
+     from sklearn.metrics import accuracy_score
+     assert accuracy_score(true_predictions, predictions) > 0.9
