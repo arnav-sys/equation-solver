9cb3a0f7e79f1574d7dd9131b604bea5edad6b59
arnav-sys <74288659+arnav-sys@users.noreply.github.com>
2022-01-05 09:23:32 +0530

    Update tests_model.py

--- tests/tests_model.py
+++ tests/tests_model.py
@@ -11,8 +11,6 @@ import tensorflow
 import pytest
 from tensorflow import keras
 
-model = keras.models.load_model("../model.h5")
-
 #making predictions
 from PIL import Image
 from itertools import groupby
@@ -82,10 +80,12 @@ elements_array = np.array(elements_list)
 'reshaping to fit model input criteria'
 elements_array = elements_array.reshape(-1, 28, 28, 1)
 
-'predicting using the model'
-elements_pred =  model.predict(elements_array)
-predictions =  np.argmax(elements_pred, axis = 1)
-true_predictions = [ 9,  8, 10 , 7,  6, 13 , 5,  4, 11,  3,  2, 12,  1 , 0]
+def test_model():
+     model = keras.models.load_model("../model.h5")
+    
+     elements_pred =  model.predict(elements_array)
+     predictions =  np.argmax(elements_pred, axis = 1)
+     true_predictions = [ 9,  8, 10 , 7,  6, 13 , 5,  4, 11,  3,  2, 12,  1 , 0]
 
-from sklearn.metrics import accuracy_score
-assert accuracy_score(true_predictions, predictions) > 0.9
+     from sklearn.metrics import accuracy_score
+     assert accuracy_score(true_predictions, predictions) > 0.9
